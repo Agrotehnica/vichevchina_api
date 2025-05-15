e474c999412d29d51dd38b79839a8c491453ec48
wolerood <wolerood@gmail.com>
2025-05-15 17:51:28 +0300

    изменен sql

--- sql_api.sql
+++ sql_api.sql
@@ -17,14 +17,15 @@ CREATE TABLE mixers (
     mixer_id VARCHAR(255) PRIMARY KEY NOT NULL
 );
 
--- Таблица для запросов
 CREATE TABLE requests (
     request_id VARCHAR(255) PRIMARY KEY NOT NULL,
     mixer_id VARCHAR(255) NOT NULL,
     bin_id VARCHAR(255) NOT NULL,
     ingredient_id VARCHAR(255) NOT NULL,
-    amount INT NOT NULL,
+    requested_amount INT NOT NULL,
+    delivered_amount INT NOT NULL,
     FOREIGN KEY (mixer_id) REFERENCES mixers(mixer_id),
     FOREIGN KEY (bin_id) REFERENCES bins(bin_id),
     FOREIGN KEY (ingredient_id) REFERENCES ingredients(ingredient_id)
 );
+
