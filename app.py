b39fcb6ca964e88359daad1ab065862d0a093525
wolerood <wolerood@gmail.com>
2025-06-11 08:59:08 +0300

    Update app.py

--- app.py
+++ app.py
@@ -67,4 +67,5 @@ async def confirm_start_loading(
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000) #  запуск в докере
+    #uvicorn.run(app, host="0.0.0.0", port=8000)  #  локальный запуск для доступа из интернета
